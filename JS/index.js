242ffbabcf50f7812a25e1d351138830eb7c268b
abhishek <amestri890@gmail.com>
2020-07-05 23:22:51 +0530

    added javascript functionality

--- JS/index.js
+++ JS/index.js
@@ -14,22 +14,22 @@ document.querySelector('.BackArrow').addEventListener('click',function(){
 document.querySelector('.SavebtnForFooter').addEventListener('click', appendingList);
 
 function appendingList(){
-    let categoryName = document.querySelector('.categoryInput').value;
-    let description = document.querySelector('.descriptionInput').value;
-    let tags = document.querySelector('.tagsInput').value;
+    let categoryName = document.querySelector('.categoryInput');
+    let description = document.querySelector('.descriptionInput');
+    let tags = document.querySelector('.tagsInput');
     let ul = document.querySelector('.categoryDataList');
     let select = document.querySelector('.parentCategory');
-    let parent = select.value
-    let newClass = categoryName.split(' ').join('');
+    let parent = select
+    let newClass = categoryName.value.split(' ').join('');
     let output = `<div class="OuterShell">
-                    <img class="dragLogo" src="drag.png"/>
+                    <img class="dragLogo" id=${newClass} ondragstart="dragStart(event)" draggable="true" ondragover="dragOver(event)" src="drag.png"/>
                     <ion-icon class="arrowIcon1" id="show${++counter}" onclick="show(id)" name="caret-forward-outline"></ion-icon>
                     <ion-icon class="arrowIcon2" id="hideshow${++counter2}" onclick="hide(id)"  name="caret-down-outline"></ion-icon>
-                    <div class="IMG"></div>
+                    <div class="IMG"  ></div>
                     <div class="ListelementsDisplay">
-                        <p class="categoryName">${categoryName}</p>
-                        <p class="descriptionForCategory"> ${description}</p>
-                        <p class="tagsForCategory"> ${tags} </p>
+                        <p class="categoryName">${categoryName.value}</p>
+                        <p class="descriptionForCategory"> ${description.value}</p>
+                        <p class="tagsForCategory"> ${tags.value} </p>
                         <p class="subCategoryOrProductsForCategory">2</p>
                         <div class="ActiveDeactiveTogglerSwitch" onclick="this.classList.toggle('active')">
                             <div class="ActiveDeactiveToggler "></div>
@@ -40,25 +40,36 @@ function appendingList(){
     let li = document.createElement('li');
     let option = document.createElement('option');
     let UL
-    if(parent == 'none'){
+    if(parent.value == 'none'){
         UL = document.createElement('ul')
         UL.setAttribute('class' , 'subCategoryHidden')
         UL.className += ' '+newClass
-        option.textContent = categoryName
+        option.textContent = categoryName.value
         select.appendChild(option)
         li.innerHTML = output;
         li.appendChild(UL)
         li.setAttribute('class' , 'categoryNameInList');
+        li.setAttribute('id',counter)
+        li.setAttribute('ondrop',"dragDrop(event)")
         ul.appendChild(li);
+        categoryName.value = "";
+        description.value = "";
+        tags.value = "";
+        parent.value = "none";
     }
     else{
         let hiddenli = document.createElement('li')
         hiddenli.innerHTML = output 
+        hiddenli.childNodes[0].childNodes[3].setAttribute('class','HideIt')
+        hiddenli.childNodes[0].childNodes[5].setAttribute('class','HideIt')
         hiddenli.setAttribute('class','subCategoryNameInList')
-        let parentEle = document.querySelector('.'+parent.split(' ').join(''));
+        let parentEle = document.querySelector('.'+parent.value.split(' ').join(''));
         parentEle.appendChild(hiddenli)
+        categoryName.value = "";
+        description.value = "";
+        tags.value = "";
+        parent.value = "none";
     }
-    console.log(li)
 }
 
 function show(id){
@@ -81,4 +92,36 @@ function hide(id){
     ArrowDown.style.display = "none"
     displayHiddenSubList.style.display = "none"
 }
+
+
+let LIs = document.querySelectorAll('.categoryNameInList');
+
+
+function dragStart(e){
+    e.dataTransfer.setData("id", e.target.id);
+}
+function dragOver(e){
+    e.preventDefault();
+}
+function dragDrop(e){
+    console.log(e.target.className)
+    e.preventDefault();
+    let data = e.dataTransfer.getData("id");
+    // let temp1 = document.getElementById(data)
+    // let temp2 = document.getElementById(e.target.id);
+
+    let first = document.getElementById(document.getElementById(data).parentElement.parentElement.id)
+    let second = document.getElementById(document.getElementById(e.target.id).parentElement.parentElement.id)
+
+    let tempr = second.innerHTML 
+    second.innerHTML = first.innerHTML
+    first.innerHTML = tempr
+
+}
+
+
+
+
+
+
 
